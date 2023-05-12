07350283f68b387052195ed89c3ff131770c7cba
Cassidy Spring <79487947+cassiecascade@users.noreply.github.com>
2023-05-12 12:12:03 -0700

    test(deno): change test library from jest to deno

--- src/result/result.test.ts
+++ src/result/result.test.ts
@@ -4,124 +4,129 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { Result } from './result'
+import { describe, it } from 'std/testing/bdd.ts'
+
+import { Result } from './result.ts'
+import { assertEquals } from 'std/testing/asserts.ts'
 
 describe('Result', () => {
   it('should be generic', () => {
     const value = Result.Ok(1)
-    expect(value.getOrElse(0)).toBe(1)
+    assertEquals(value.getOrElse(0), 1)
 
     const value2 = Result.Ok('1')
-    expect(value2.getOrElse('0')).toBe('1')
+    assertEquals(value2.getOrElse('0'), '1')
 
     const value3 = Result.Ok({ a: 1 })
-    expect(value3.getOrElse({ a: 0 })).toEqual({ a: 1 })
+    assertEquals(value3.getOrElse({ a: 0 }), { a: 1 })
 
     const value4 = Result.Error(1)
-    expect(value4.getOrElse(0)).toBe(0)
+    assertEquals(value4.getOrElse(0), 0)
 
     const value5 = Result.Error('1')
-    expect(value5.getOrElse('0')).toBe('0')
+    assertEquals(value5.getOrElse('0'), '0')
 
     const value6 = Result.Error({ a: 1 })
-    expect(value6.getOrElse({ a: 0 })).toEqual({ a: 0 })
+    assertEquals(value6.getOrElse({ a: 0 }), { a: 0 })
   })
 
   describe('isOk', () => {
     it('should return true if the value is not null', () => {
       const value = Result.Ok(1)
-      expect(value.isOk()).toBe(true)
+      assertEquals(value.isOk(), true)
     })
 
     it('should return false if the value is null', () => {
       const value = Result.Error(1)
-      expect(value.isOk()).toBe(false)
+      assertEquals(value.isOk(), false)
     })
   })
 
   describe('isError', () => {
     it('should return false if the value is not null', () => {
       const value = Result.Ok(1)
-      expect(value.isError()).toBe(false)
+      assertEquals(value.isError(), false)
     })
 
     it('should return true if the value is null', () => {
       const value = Result.Error(1)
-      expect(value.isError()).toBe(true)
+      assertEquals(value.isError(), true)
     })
   })
 
   describe('match', () => {
     it('should return the result of the ok function if it is not null', () => {
       const value = Result.Ok(1)
-      expect(
+      assertEquals(
         value.match(
           (value) => value,
           () => 0,
         ),
-      ).toBe(1)
+        1,
+      )
     })
 
     it('should return the result of the error function if it is null', () => {
       const value = Result.Error(1)
-      expect(
+      assertEquals(
         value.match(
           (value) => value,
           () => 0,
         ),
-      ).toBe(0)
+        0,
+      )
     })
   })
 
   describe('matchWith', () => {
     it('should return the result of the ok function if it is not null', () => {
       const value = Result.Ok(1)
-      expect(value.matchWith({ ok: (value) => value, error: () => 0 })).toBe(1)
+      assertEquals(value.matchWith({ ok: (value) => value, error: () => 0 }), 1)
     })
 
     it('should return the result of the error function if it is null', () => {
       const value = Result.Error(1)
-      expect(value.matchWith({ ok: (value) => value, error: () => 0 })).toBe(0)
+      assertEquals(value.matchWith({ ok: (value) => value, error: () => 0 }), 0)
     })
   })
 
   describe('getOrElse', () => {
     it('should return the value if it is not null', () => {
       const value = Result.Ok(1)
-      expect(value.getOrElse(0)).toBe(1)
+      assertEquals(value.getOrElse(0), 1)
     })
 
     it('should return the default value if it is null', () => {
       const value = Result.Error(1)
-      expect(value.getOrElse(0)).toBe(0)
+      assertEquals(value.getOrElse(0), 0)
     })
   })
 
   describe('getOrDo', () => {
     it('should return the value if it is not null', () => {
       const value = Result.Ok(1)
-      expect(value.getOrDo(() => 0)).toBe(1)
+      assertEquals(value.getOrDo(() => 0), 1)
     })
 
     it('should return the default value if it is null', () => {
       const value = Result.Error(1)
-      expect(value.getOrDo(() => 0)).toBe(0)
+      assertEquals(value.getOrDo(() => 0), 0)
     })
   })
 
   describe('Ok', () => {
     it('should return a result with the value', () => {
       const value = Result.Ok(1)
-      expect(value.isOk()).toBe(true)
-      expect(value.getOrElse(0)).toBe(1)
+      assertEquals(value.isOk(), true)
+      assertEquals(value.getOrElse(0), 1)
     })
   })
 
   describe('Error', () => {
     it('should return a result with the error', () => {
       const value = Result.Error(1)
-      expect(value.isError()).toBe(true)
-      expect(value.getOrElse(0)).toBe(0)
+      assertEquals(value.isError(), true)
+      assertEquals(value.getOrElse(0), 0)
     })
   })
 })
