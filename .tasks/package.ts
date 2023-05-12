d75cc5d3928612136d606f034e1db54aba7caa40
Cassidy Spring <79487947+cassiecascade@users.noreply.github.com>
2023-05-12 14:42:53 -0700

    refactor(deno): use deno dnt to create npm package
    This fundamentally makes the entire development flow of folklore deno
    based, which is honestly really nice.

---
+++ .tasks/package.ts
@@ -0,0 +1,30 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { build, emptyDir } from 'dnt/mod.ts'
+
+import { pkg } from '../package.ts'
+
+await emptyDir('./.dist')
+
+await build({
+  entryPoints: ['./mod.ts'],
+  outDir: './.dist',
+  test: false,
+  esModule: true,
+  scriptModule: false,
+  skipSourceOutput: true,
+  declaration: 'inline',
+  shims: {
+    deno: 'dev',
+  },
+  package: pkg,
+  postBuild() {
+    Deno.copyFileSync('license.md', '.dist/license.md')
+    Deno.copyFileSync('readme.md', '.dist/readme.md')
+    Deno.removeSync('.dist/.npmignore')
+  },
+})
---
+++ package.ts
@@ -0,0 +1,21 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+export const pkg = {
+  name: 'folklore',
+  version: '0.0.1',
+  description: 'a retelling of folktale, early, use at your own peril',
+  author: 'Cassidy Spring <79487947+cassiecascade@users.noreply.github.com>',
+  license: 'MPL-2.0',
+  homepage: 'https://github.com/cassiecascade/folklore#readme',
+  repository: {
+    'type': 'git',
+    'url': 'git+https://github.com/cassiecascade/folklore.git',
+  },
+  bugs: {
+    'url': 'https://github.com/cassiecascade/folklore/issues',
+  },
+}
