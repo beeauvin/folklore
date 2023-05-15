7f2b4104801b8b331be8bf70d65bc58f63e5f946
Cassidy Spring <79487947+cassiecascade@users.noreply.github.com>
2023-05-14 17:20:34 -0700

    feat(maybe): stabilize maybe to folktales api
    This isn't a 1-to-1 mapping. Global Just and Nothing functions don't
    exist, static methods are pascal case, etc. See #13 for more on this.
    Still, it's a good base to work from and make notes on why future
    changes happen.
    
    Closes #8

--- maybe/maybe.test.ts
+++ maybe/maybe.test.ts
@@ -4,153 +4,193 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { describe, it } from 'std/testing/bdd.ts'
+import { Just, Maybe } from './maybe.ts'
+import { assertSpyCall, assertSpyCalls, Spy, spy } from 'std/testing/mock.ts'
+import { beforeEach, describe, it } from 'std/testing/bdd.ts'
 
-import { Maybe } from './maybe.ts'
 import { assertEquals } from 'std/testing/asserts.ts'
 
 describe('Maybe', () => {
-  it('should be generic', () => {
-    const value = Maybe.Just(1)
-    assertEquals(value.getOrElse(0), 1)
+  const maybeConditionals = [1, '1', [1, 2, 3], { a: 1 }, () => 1]
+  const maybeJust = maybeConditionals.map(Maybe.Just)
+  const maybeJustWithValue = maybeConditionals.map((value) => ({ value, maybe: Maybe.Just(value) }))
+  const maybeNothing = [Maybe.FromNullable(null), Maybe.FromNullable(undefined), Maybe.Nothing()]
 
-    const value2 = Maybe.Just('1')
-    assertEquals(value2.getOrElse('0'), '1')
+  let justHandlerSpy: Spy
+  let nothingHandlerSpy: Spy
 
-    const value3 = Maybe.Just({ a: 1 })
-    assertEquals(value3.getOrElse({ a: 0 }), { a: 1 })
+  beforeEach(() => {
+    justHandlerSpy = spy(<T>(value: T) => {
+      return value
+    })
+    nothingHandlerSpy = spy(() => {})
   })
 
-  describe('isJust', () => {
-    it('should return true if the value is not null', () => {
-      const value = Maybe.Just(1)
-      assertEquals(value.isJust(), true)
+  describe('isJust()', () => {
+    it('should return true if maybe is just (x)', () => {
+      maybeJust.forEach((maybe) => assertEquals(maybe.isJust(), true))
     })
 
-    it('should return false if the value is null', () => {
-      const value = Maybe.Nothing()
-      assertEquals(value.isJust(), false)
-    })
-  })
-
-  describe('isNothing', () => {
-    it('should return false if the value is not null', () => {
-      const value = Maybe.Just(1)
-      assertEquals(value.isNothing(), false)
-    })
-
-    it('should return true if the value is null', () => {
-      const value = Maybe.Nothing()
-      assertEquals(value.isNothing(), true)
+    it('should return false if maybe is nothing', () => {
+      maybeNothing.forEach((maybe) => assertEquals(maybe.isJust(), false))
     })
   })
 
-  describe('match', () => {
-    it('should return the result of the just function if it is not null', () => {
-      const value = Maybe.Just(1)
-      assertEquals(
-        value.match(
-          (value) => value,
-          () => 0,
-        ),
-        1,
-      )
+  describe('isNothing()', () => {
+    it('should return false if maybe is just (x)', () => {
+      maybeJust.forEach((maybe) => assertEquals(maybe.isNothing(), false))
     })
 
-    it('should return the result of the nothing function if it is null', () => {
-      const value = Maybe.Nothing()
-      assertEquals(
-        value.match(
-          (value) => value,
-          () => 0,
-        ),
-        0,
-      )
+    it('should return true if maybe is nothing', () => {
+      maybeNothing.forEach((maybe) => assertEquals(maybe.isNothing(), true))
     })
   })
 
-  describe('matchWith', () => {
-    it('should return the result of the just function if it is not null', () => {
-      const value = Maybe.Just(1)
-      assertEquals(value.matchWith({ just: (value) => value, nothing: () => 0 }), 1)
+  describe('matchWith()', () => {
+    it('should return the result of the Just handler when maybe is just (x)', () => {
+      maybeJustWithValue.forEach((wrapped, index) => {
+        const { maybe, value } = wrapped
+        maybe.matchWith({ Just: justHandlerSpy, Nothing: nothingHandlerSpy })
+        assertSpyCalls(justHandlerSpy, index + 1)
+        assertSpyCalls(nothingHandlerSpy, 0)
+        assertSpyCall(justHandlerSpy, index, {
+          args: [value],
+          returned: value,
+        })
+      })
     })
 
-    it('should return the result of the nothing function if it is null', () => {
-      const value = Maybe.Nothing()
-      assertEquals(value.matchWith({ just: (value) => value, nothing: () => 0 }), 0)
-    })
-  })
-
-  describe('getOrElse', () => {
-    it('should return the value if it is not null', () => {
-      const value = Maybe.Just(1)
-      assertEquals(value.getOrElse(0), 1)
-    })
-
-    it('should return the default value if it is null', () => {
-      const value = Maybe.Nothing()
-      assertEquals(value.getOrElse(0), 0)
-    })
-  })
-
-  describe('getOrDo', () => {
-    it('should return the value if it is not null', () => {
-      const value = Maybe.Just(1)
-      assertEquals(value.getOrDo(() => 0), 1)
-    })
-
-    it('should return the result of the action if it is null', () => {
-      const value = Maybe.Nothing()
-      assertEquals(value.getOrDo(() => 0), 0)
+    it('should return the result of the Nothing handler when maybe is nothing', () => {
+      maybeNothing.forEach((maybe, index) => {
+        maybe.matchWith({ Just: justHandlerSpy, Nothing: nothingHandlerSpy })
+        assertSpyCalls(justHandlerSpy, 0)
+        assertSpyCalls(nothingHandlerSpy, index + 1)
+        assertSpyCall(nothingHandlerSpy, index, {})
+      })
     })
   })
 
-  describe('getOrThrow', () => {
-    it('should return the value if it is not null', () => {
-      const value = Maybe.Just(1)
-      assertEquals(value.getOrThrow(), 1)
+  describe('getOrElse()', () => {
+    it('should return the wrapped value if maybe is just (x)', () => {
+      maybeJustWithValue.forEach((wrapped) => {
+        const { maybe, value } = wrapped
+        assertEquals(maybe.getOrElse(0), value)
+      })
     })
 
-    it('should throw an error if it is null', () => {
-      const value = Maybe.Nothing()
-      assertEquals(value.getOrElse(0), 0)
-    })
-  })
-
-  describe('FromNullable', () => {
-    it('should return a Just if the value is not null', () => {
-      const value = Maybe.FromNullable(1)
-      assertEquals(value.getOrElse(0), 1)
-    })
-
-    it('should return a Nothing if the value is null', () => {
-      function testNullable(): number | null {
-        return null
-      }
-      const value = Maybe.FromNullable(testNullable())
-      assertEquals(value.getOrElse(0), 0)
-    })
-
-    it('should return a Nothing if the value is undefined', () => {
-      function testNullable(): number | undefined {
-        return undefined
-      }
-      const value = Maybe.FromNullable(testNullable())
-      assertEquals(value.getOrElse(0), 0)
+    it('should return the default value if maybe is nothing', () => {
+      maybeNothing.forEach((maybe) => {
+        assertEquals(maybe.getOrElse(0), 0)
+      })
     })
   })
 
-  describe('Just', () => {
-    it('should return a Just', () => {
-      const value = Maybe.Just(1)
-      assertEquals(value.getOrElse(0), 1)
+  describe('orElse()', () => {
+    it('should return the maybe if maybe is just (x)', () => {
+      maybeJust.forEach((maybe) => {
+        assertEquals(maybe.orElse(nothingHandlerSpy), maybe)
+        assertSpyCalls(nothingHandlerSpy, 0)
+      })
+    })
+
+    it('should return the call the handler if maybe is nothing', () => {
+      maybeNothing.forEach((maybe, index) => {
+        maybe.orElse(nothingHandlerSpy)
+        assertSpyCalls(nothingHandlerSpy, index + 1)
+      })
     })
   })
 
-  describe('Nothing', () => {
-    it('should return a Nothing', () => {
-      const value = Maybe.Nothing()
-      assertEquals(value.getOrElse(0), 0)
+  describe('map()', () => {
+    it('should return a new maybe with the result of the handler if maybe is just (x)', () => {
+      maybeJustWithValue.forEach((wrapped, index) => {
+        const { maybe, value } = wrapped
+        const result = maybe.map(justHandlerSpy)
+        assertEquals(result.isJust(), true)
+        assertEquals(result.getOrElse(0), value)
+        assertSpyCalls(justHandlerSpy, index + 1)
+        assertSpyCall(justHandlerSpy, index, {
+          args: [value],
+          returned: value,
+        })
+      })
+    })
+
+    it('should return nothing if maybe is nothing', () => {
+      maybeNothing.forEach((maybe) => {
+        const result = maybe.map(justHandlerSpy)
+        assertEquals(result.isNothing(), true)
+        assertEquals(result.getOrElse(0), 0)
+        assertSpyCalls(justHandlerSpy, 0)
+      })
+    })
+  })
+
+  describe('chain()', () => {
+    it('should return the result of the handler if maybe is just (x)', () => {
+      maybeJustWithValue.forEach((wrapped) => {
+        justHandlerSpy = spy(<T>(value: Just<T>) => {
+          return Maybe.Just(value)
+        })
+        const { maybe, value } = wrapped
+        const result = maybe.chain(justHandlerSpy)
+        assertEquals(result.isJust(), true)
+        assertEquals(result.getOrElse(0), value)
+        assertSpyCalls(justHandlerSpy, 1)
+        assertSpyCall(justHandlerSpy, 0, {
+          args: [value],
+          returned: Maybe.Just(value),
+        })
+      })
+    })
+
+    it('should return nothing if maybe is nothing', () => {
+      maybeNothing.forEach((maybe) => {
+        const result = maybe.chain(justHandlerSpy)
+        assertEquals(result.isNothing(), true)
+        assertEquals(result.getOrElse(0), 0)
+        assertSpyCalls(justHandlerSpy, 0)
+      })
+    })
+  })
+
+  describe('HasInstance()', () => {
+    it('should return true if value is an instance of Maybe and falso otherwise', () => {
+      maybeJustWithValue.forEach((wrapped) => {
+        const { maybe, value } = wrapped
+        assertEquals(Maybe.HasInstance(maybe), true)
+        assertEquals(Maybe.HasInstance(value), false)
+      })
+
+      maybeNothing.forEach((maybe) => {
+        assertEquals(Maybe.HasInstance(maybe), true)
+      })
+    })
+  })
+
+  describe('FromNullable()', () => {
+    it('should return a Just if value is not null or undefined and Nothing otherwise', () => {
+      maybeConditionals.forEach((value) => {
+        assertEquals(Maybe.FromNullable(value), Maybe.Just(value))
+      })
+      assertEquals(Maybe.FromNullable(null), Maybe.Nothing())
+      assertEquals(Maybe.FromNullable(undefined), Maybe.Nothing())
+    })
+  })
+
+  describe('Just()', () => {
+    it('should return a new Maybe with the value wrapped', () => {
+      maybeConditionals.forEach((value) => {
+        assertEquals(Maybe.HasInstance(Maybe.Just(value)), true)
+        assertEquals(Maybe.Just(value).getOrElse(0), value)
+      })
+    })
+  })
+
+  describe('Nothing()', () => {
+    it('should return a new Maybe with nothing', () => {
+      assertEquals(Maybe.HasInstance(Maybe.Nothing()), true)
+      assertEquals(Maybe.Nothing().getOrElse(0), 0)
     })
   })
 })
