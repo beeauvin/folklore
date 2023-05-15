8b708321a14dfd91fee85b8362d3f5ab8cbf95f3
Cassidy Spring <79487947+cassiecascade@users.noreply.github.com>
2023-05-14 18:53:04 -0700

    feat(result): stabilize result with folktales api (#15)
    * feat(result): stabilize result with folktales api
    
    Similar to Maybe this has some differences. Static methods, pascal case,
    etc. There's also no direct tests. Most of the functionality is
    duplicated from Maybe and I feel like thos etests apply. The little bit
    that isn't needs tests but this will have to come in a future iteration
    after I decide on which path I will be going down to reduce duplication.
    There are mutliple ways to solve for that and I haven't made up my mind
    yet.
    
    Closes #9
    
    * style(fmt): run formatter

--- result/result.ts
+++ result/result.ts
@@ -4,8 +4,11 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-export class Result<T, E = string | Error> {
-  private constructor(private readonly value: T, private readonly error: E) {}
+export type Ok<Type> = Type extends Error ? never : Type
+export type Err = string | Error
+
+export class Result<Type> {
+  private constructor(private readonly value: Ok<Type>, private readonly error: Err) {}
 
   public isOk(): boolean {
     return this.value != null
@@ -15,34 +18,83 @@ export class Result<T, E = string | Error> {
     return this.error != null
   }
 
-  public match<U>(ok: (value: T) => U, error: (error: E) => U): U {
-    if (this.isOk()) return ok(this.value)
-    else return error(this.error)
+  public matchWith<OkReturnType, ErrorReturnType>(pattern: {
+    Ok: (value: Ok<Type>) => OkReturnType
+    Error: (error: Err) => ErrorReturnType
+  }): OkReturnType | ErrorReturnType {
+    if (this.isOk()) return pattern.Ok(this.value!)
+    else return pattern.Error(this.error)
   }
 
-  public matchWith<U>(pattern: { ok: (value: T) => U; error: (error: E) => U }): U {
-    return this.match(pattern.ok, pattern.error)
+  public getOrElse(defaultValue: Ok<Type>): Ok<Type> {
+    return this.matchWith({
+      Ok: (value) => value,
+      Error: () => defaultValue,
+    })
   }
 
-  public getOrElse(defaultValue: T): T {
-    return this.match(
-      (value) => value,
-      () => defaultValue,
-    )
+  public orElse<HandlerType>(
+    handler: (value: Result<Type>) => Result<HandlerType>,
+  ): Result<Type> | Result<HandlerType> {
+    return this.matchWith({
+      Ok: (value) => Result.Ok(value),
+      Error: (error) => handler(Result.Error(error)),
+    })
   }
 
-  public getOrDo(action: () => T): T {
-    return this.match(
-      (value) => value,
-      () => action(),
-    )
+  public merge(): Type | Err {
+    return this.matchWith({
+      Ok: (value) => value,
+      Error: (error) => error,
+    })
   }
 
-  public static Ok<T>(value: T): Result<T, never> {
-    return new Result<T, never>(value, undefined as never)
+  public mapError(error: Err): Result<Type> {
+    return this.matchWith({
+      Ok: (value) => Result.Ok(value),
+      Error: () => Result.Error(error),
+    })
   }
 
-  public static Error<T, E>(error: E): Result<T, typeof error> {
-    return new Result<T, E>(undefined as never, error)
+  public map<HandlerType>(handler: (value: Type) => Ok<HandlerType>): Result<HandlerType> {
+    return this.matchWith({
+      Ok: (value) => Result.Ok(handler(value)),
+      Error: (error) => Result.Error(error),
+    })
+  }
+
+  public chain<HandlerType>(handler: (value: Type) => Result<HandlerType>): Result<HandlerType> {
+    return this.matchWith({
+      Ok: (value) => handler(value),
+      Error: (error) => Result.Error(error),
+    })
+  }
+
+  public static HasInstance<Type>(value: Type): boolean {
+    return value instanceof Result
+  }
+
+  public static Try<Type>(method: () => Ok<Type>): Result<Type> {
+    try {
+      return Result.Ok(method())
+    } catch (error) {
+      return Result.Error(error)
+    }
+  }
+
+  public static async FromPromise<Type>(promise: () => Promise<Ok<Type>>): Promise<Result<Type>> {
+    try {
+      return Result.Ok(await promise())
+    } catch (error) {
+      return Result.Error(error)
+    }
+  }
+
+  public static Ok<Type>(value: Ok<Type>): Result<Type> {
+    return new Result<Type>(value, undefined as never)
+  }
+
+  public static Error<Type>(error: Err): Result<Type> {
+    return new Result<Type>(undefined as never, error)
   }
 }
--- unstable/unstable.ts
+++ unstable/unstable.ts
@@ -1,2 +1,3 @@
 export { Future } from './future/future.ts'
 export { Maybe } from './maybe/maybe.ts'
+export { Result } from './result/result.ts'
