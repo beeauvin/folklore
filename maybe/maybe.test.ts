a3fd26587b819f223b8fca4681b79a7ac56bd81e
Bee <79487947+beeauvin@users.noreply.github.com>
2024-06-05 10:12:11 -0700

    chore(jsr): update deno.jsonc (#55)
    * chore(jsr): update deno.jsonc
    
    Ugh, sad that the publish script doesn't check for `jsr.json` first.
    
    * test(lint): update test to pass linter

--- maybe/maybe.test.ts
+++ maybe/maybe.test.ts
@@ -4,7 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { assertSpyCall, assertSpyCalls, Spy, spy } from 'std/testing/mock.ts'
+import { assertSpyCall, assertSpyCalls, type Spy, spy } from 'std/testing/mock.ts'
 import { beforeEach, describe, it } from 'std/testing/bdd.ts'
 
 import { Maybe } from './maybe.ts'
