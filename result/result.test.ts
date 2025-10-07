9e6d45eaf559504e279af17173f60f7de535020f
Cassidy Spring (Bee) <79487947+beeauvin@users.noreply.github.com>
2025-10-07 09:50:48 -0700

    refactor(base): [breaking] remove abstract base class
    The reusability of the base class isn't as helpful as removing the cognitive overhead of it's existence. Further, in line with generally having one way to do things, this removes the instanced (non static) version of instance comparison.
    
    This is a breaking change.

--- result/result.test.ts
+++ result/result.test.ts
@@ -5,13 +5,21 @@
  */
 
 import { Result } from './result.ts'
-import { Base } from '../abstract/base.ts'
 import { assertEquals } from '@std/assert'
 
 Deno.test('Result', async (test) => {
-  await test.step('extends base', () => {
-    assertEquals(Result.prototype instanceof Base, true)
-    assertEquals(Base.HasInstance(Result.Ok(1)), true)
-    assertEquals(Base.HasInstance(Result.Error('')), true)
+  await test.step('HasInstance()', async (t) => {
+    await t.step('should return true for Result instances', () => {
+      assertEquals(Result.HasInstance(Result.Ok(5)), true)
+      assertEquals(Result.HasInstance(Result.Error('oops')), true)
+    })
+
+    await t.step('should return false for non-Result values', () => {
+      assertEquals(Result.HasInstance(5), false)
+      assertEquals(Result.HasInstance(null), false)
+      assertEquals(Result.HasInstance(undefined), false)
+      assertEquals(Result.HasInstance({}), false)
+      assertEquals(Result.HasInstance('string'), false)
+    })
   })
 })
