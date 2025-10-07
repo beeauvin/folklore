0fef24b914a329f101ed8b6bd48c16015b3db5a3
Cassidy Spring (Bee) <79487947+beeauvin@users.noreply.github.com>
2025-10-07 11:13:04 -0700

    refactor: remove operators
    The intent of this was to reduce duplication but without better composability or reverting to polymorphism it made pseudo single source of truth at the cost of more cognitive load of the library.
    
    I'm reverting that change.

--- maybe/maybe.ts
+++ maybe/maybe.ts
@@ -8,11 +8,6 @@ import type { Nullable } from '../utility/types.ts'
 import { is_nothing } from '../comparison/is-nothing.ts'
 import { is_something } from '../comparison/is-something.ts'
 import { is_instance_of } from '../comparison/is-instance-of.ts'
-import { get_or_else } from '../operators/get-or-else.ts'
-import { map_monad } from '../operators/map-monad.ts'
-import { chain_monad } from '../operators/chain-monad.ts'
-import { or_else_monad } from '../operators/or-else-monad.ts'
-import { match_with } from '../operators/match-with.ts'
 
 export class Maybe<Type> {
   private constructor(private readonly value: Nullable<Type> = undefined) {}
@@ -33,13 +28,11 @@ export class Maybe<Type> {
     Just: (value: NonNullable<Type>) => JustReturnType
     Nothing: () => NothingReturnType
   }): JustReturnType | NothingReturnType {
-    return match_with(
-      this.isJust(),
-      this.value!,
-      undefined,
-      pattern.Just,
-      pattern.Nothing,
-    )
+    if (this.isJust()) {
+      return pattern.Just(this.value!)
+    } else {
+      return pattern.Nothing()
+    }
   }
 
   /**
@@ -58,37 +51,31 @@ export class Maybe<Type> {
   }
 
   public getOrElse(defaultValue: NonNullable<Type>): NonNullable<Type> {
-    return get_or_else(this.isJust(), this.value!, defaultValue)
+    return this.isJust() ? this.value! : defaultValue
   }
 
   public orElse<HandlerType>(handler: () => Maybe<HandlerType>): Maybe<Type> | Maybe<HandlerType> {
-    return or_else_monad(
-      this.isJust(),
-      this.value!,
-      handler,
-      (value: NonNullable<Type>) => Maybe.Just(value),
-    ) as Maybe<Type> | Maybe<HandlerType>
+    if (this.isJust()) {
+      return Maybe.Just(this.value!)
+    } else {
+      return handler()
+    }
   }
 
   public map<HandlerType>(handler: (value: Type) => NonNullable<HandlerType>): Maybe<HandlerType> {
-    return map_monad(
-      this.isJust(),
-      this.value!,
-      undefined,
-      handler,
-      (value: NonNullable<HandlerType>) => Maybe.Just(value),
-      () => Maybe.Nothing<HandlerType>(),
-    ) as Maybe<HandlerType>
+    if (this.isJust()) {
+      return Maybe.Just(handler(this.value!))
+    } else {
+      return Maybe.Nothing<HandlerType>()
+    }
   }
 
   public chain<HandlerType>(handler: (value: Type) => Maybe<HandlerType>): Maybe<HandlerType> {
-    return chain_monad(
-      this.isJust(),
-      this.value!,
-      undefined,
-      handler,
-      () => Maybe.Nothing<HandlerType>(),
-    ) as Maybe<HandlerType>
+    if (this.isJust()) {
+      return handler(this.value!)
+    } else {
+      return Maybe.Nothing<HandlerType>()
+    }
   }
 
   public static FromNullable<Type>(value: Nullable<Type>): Maybe<Type> {
